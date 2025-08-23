2f5b744bc475fdb8d34599fa6489280e95097a18
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-23 11:12:11 -0800

    Write hashes to file

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -3,8 +3,8 @@
 import os
 from hashlib import sha256
 
+hashes = []
 current_directory = os.getcwd()
-
 ignore_list = [".git", ".github", ".gitmodules", "hashes.txt", "README.md"]
 
 def hash(data_str):
@@ -13,14 +13,40 @@ def hash(data_str):
 	hex_digest = sha256_hash_object.hexdigest()
 	return hex_digest
 
-hashes = []
+def read_file(path):
+	try:
+		with open(path, 'r', encoding='utf-8') as file:
+			return file.read()
+	except FileNotFoundError:
+		pass
+	except Exception as e:
+		pass
 
-def hash_files_recursive(path):
-	print(path, hash(path))
+def hash_files_recursive(dir_path):
+	for file_or_dir in os.listdir(current_directory + "/" + dir_path):
+		local_path = dir_path + "/" + file_or_dir
+		current_path = current_directory + "/" + local_path
 
-print("dirs")
+		if os.path.isfile(current_path):
+			file_contents = read_file(current_path)
+			if file_contents:
+				file_hash = hash(file_contents)
+				hashes.append((file_hash, local_path))
+
+		if os.path.isdir(current_path):
+			hash_files_recursive(local_path)
+
+
 for file_or_dir in os.listdir():
 	if file_or_dir in ignore_list:
-		print("ignored", dir)
 		continue
-	hash_files_recursive(current_directory+"/"+file_or_dir)
+	hash_files_recursive(file_or_dir)
+
+# clear hashes.txt of contents
+open("hashes.txt", "w").close()
+
+# append all the hashes
+with open("hashes.txt", "a") as f:
+	for file_hash, file_path in hashes:
+		f.write(file_hash+" "+file_path+"\n")
+		print(file_hash, file_path)

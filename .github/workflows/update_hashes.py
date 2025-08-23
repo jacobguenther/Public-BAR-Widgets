3835ff415abee06cd30e21e880a71fa951adebf4
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-23 10:15:06 -0800

    test hashing dir names

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import os
+from hashlib import sha256
 
 current_directory = os.getcwd()
 
 ignore_dirs = [".git", ".github"]
+
+def hash(data_str):
+	data_bytes = data_str.encode('utf-8')
+	sha256_hash_object = sha256(data_bytes)
+	hex_digest = sha256_hash_object.hexdigest()
+	return hex_digest
 
 for subdirs, dirs, files in os.walk(current_directory):
 	# print(subdirs, dirs, files)
@@ -16,7 +23,7 @@ for subdirs, dirs, files in os.walk(current_directory):
 	for dir in dirs:
 		if dir in ignore_dirs:
 			continue
-		print(dir)
+		print(dir, hash(dir))
 
 	# print("files")
 	# for file in files:

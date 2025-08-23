3ad2bfb4e0dcd7640563fcdd3613892286aea32d
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-23 09:59:34 -0800

    print individually

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -5,4 +5,15 @@ import os
 current_directory = os.getcwd()
 
 for subdirs, dirs, files in os.walk(current_directory):
-	print(subdirs, dirs, files)
+	# print(subdirs, dirs, files)
+	print("subdirs")
+	for subdir in subdirs:
+		print(subdir)
+
+	print("dirs")
+	for dir in dirs:
+		print(dir)
+
+	print("files")
+	for file in files:
+		print(file)

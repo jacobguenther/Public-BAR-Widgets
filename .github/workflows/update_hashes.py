e9e4eec9da123805ab003faaceab52d33d338ee0
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-23 10:04:18 -0800

    ignore git/github folders

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -4,16 +4,20 @@ import os
 
 current_directory = os.getcwd()
 
+ignore_dirs = [".git", ".github"]
+
 for subdirs, dirs, files in os.walk(current_directory):
 	# print(subdirs, dirs, files)
-	print("subdirs")
-	for subdir in subdirs:
-		print(subdir)
+	# print("subdirs")
+	# for subdir in subdirs:
+	# 	print(subdir)
 
 	print("dirs")
 	for dir in dirs:
+		if dir in ignore_dirs:
+			continue
 		print(dir)
 
-	print("files")
-	for file in files:
-		print(file)
+	# print("files")
+	# for file in files:
+	# 	print(file)

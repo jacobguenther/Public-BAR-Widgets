34dbb439f285d44246a03b0e0b91b8eaeeb2e138
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-23 11:42:20 -0800

    cleanup

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -23,38 +23,26 @@ def read_file(path):
 		pass
 
 def hash_files_recursive(dir_path):
-	print("hash_files_recursive", dir_path)
-
 	for file_or_dir in os.listdir(dir_path):
 		local_path = dir_path + "/" + file_or_dir
 		current_path = local_path
-
-		print(current_path)
 
 		if os.path.isfile(current_path):
 			print("file", current_path)
 			file_contents = read_file(current_path)
 			if file_contents:
-				file_hash = hash(file_contents)
 				hashes.append((file_hash, local_path))
 
 		if os.path.isdir(current_path):
-			print("dir", current_path)
 			hash_files_recursive(local_path)
 
-print("start")
-
 for file_or_dir in os.listdir(current_directory):
-	print(file_or_dir)
 	if file_or_dir in ignore_list:
 		continue
 	hash_files_recursive(file_or_dir)
 
-# clear hashes.txt of contents
 open(current_directory+"/"+"hashes.txt", "w").close()
 
-# append all the hashes
 with open(current_directory+"/"+"hashes.txt", "a") as f:
 	for file_hash, file_path in hashes:
 		f.write(file_hash+" "+file_path+"\n")
-		print(file_hash, file_path)

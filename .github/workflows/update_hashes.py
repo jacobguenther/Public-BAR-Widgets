a1977d209d907d8bc0314ea98a2a74b5b015af31
jacobguenther <jacobrwguenther@protonmail.com>
2025-08-24 06:44:34 -0800

    clean up.

--- .github/workflows/update_hashes.py
+++ .github/workflows/update_hashes.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-from hashlib import sha256
+import hashlib
 
 hashes = []
-current_directory = os.getcwd()
-ignore_list = [".git", ".github", ".gitmodules", "hashes.txt", "README.md"]
+ignore_list = [".git", ".github", ".gitignore"]
 
 def hash(data_str):
 	data_bytes = data_str.encode('utf-8')
-	sha256_hash_object = sha256(data_bytes)
+	sha256_hash_object = hashlib.sha256(data_bytes)
 	hex_digest = sha256_hash_object.hexdigest()
 	return hex_digest
 
@@ -24,25 +23,28 @@ def read_file(path):
 
 def hash_files_recursive(dir_path):
 	for file_or_dir in os.listdir(dir_path):
-		local_path = dir_path + "/" + file_or_dir
-		current_path = local_path
+		current_path = dir_path + "/" + file_or_dir
 
-		if os.path.isfile(current_path):
+		if os.path.isfile(current_path) and file_or_dir not in ignore_list:
 			file_contents = read_file(current_path)
 			if file_contents:
 				file_hash = hash(file_contents)
-				hashes.append((file_hash, local_path))
+				hashes.append((file_hash, current_path))
 
-		if os.path.isdir(current_path):
-			hash_files_recursive(local_path)
+		if os.path.isdir(current_path) and file_or_dir not in ignore_list:
+			hash_files_recursive(current_path)
 
-for file_or_dir in os.listdir(current_directory):
-	if file_or_dir in ignore_list:
-		continue
-	hash_files_recursive(file_or_dir)
+def hash_files():
+	current_directory = os.getcwd()
+	for file_or_dir in os.listdir(current_directory):
+		if os.path.isdir(file_or_dir) and file_or_dir not in ignore_list:
+			hash_files_recursive(file_or_dir)
 
-open(current_directory+"/"+"hashes.txt", "w").close()
+def write_hashes():
+	open("hashes.txt", "w").close()
+	with open("hashes.txt", "a") as f:
+		for file_hash, file_path in hashes:
+			f.write(file_hash+" "+file_path+"\n")
 
-with open(current_directory+"/"+"hashes.txt", "a") as f:
-	for file_hash, file_path in hashes:
-		f.write(file_hash+" "+file_path+"\n")
+hash_files()
+write_hashes()
